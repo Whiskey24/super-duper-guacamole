8d794d4a4434eed25345f6fa76b383de0913b923
Whiskey24 <bert@santema.eu>
2024-01-08 21:35:32 +0100

    Add AppConfig and AWSConfig interfaces

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,9 @@
 import fs from 'fs';
 import path from 'path';
 import yaml from 'js-yaml';
+import { AppConfig } from './types';
 
 const configPath = path.join(__dirname, '../config.yml');
-const config = yaml.load(fs.readFileSync(configPath, 'utf8'));
+const config = yaml.load(fs.readFileSync(configPath, 'utf8')) as AppConfig;
 
 export default config;
--- src/types/index.ts
+++ src/types/index.ts
@@ -39,3 +39,22 @@ export interface ScrapedData {
   id: number;
   data: string;
 }
+
+export interface AWSConfig {
+  name: string;
+  runtime: string;
+  stage: string;
+  region: string;
+  database: {
+    host: string;
+    user: string;
+    password: string;
+    database: string;
+    port: number;
+  };
+}
+
+export interface AppConfig {
+  aws: AWSConfig;
+  telegramBotToken: string;
+}
