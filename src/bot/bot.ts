550883a8232b041edd4d4428d72aa0630b851903
Whiskey24 <bert@santema.eu>
2024-02-02 00:30:15 +0100

    Load json from lambda, write subscription to s3, put webhook in its own module

--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -1,7 +1,8 @@
 import { Bot, webhookCallback, InlineKeyboard, Context, MiddlewareFn } from 'grammy'
-import * as webScraping from './tasks/webScraping';
-import * as s3 from './database/s3Storage';
+import * as webScraping from '../tasks/webScraping';
+import * as s3 from '../database/s3Storage';
 import * as fs from 'fs';
+import subscriptions from '../subscriptions.json'; // allows loading a json file included in the source
 
 // Check if process.env.TELEGRAM_TOKEN is undefined or empty
 if (!process.env.TELEGRAM_TOKEN) {
@@ -11,14 +12,16 @@ if (!process.env.TELEGRAM_TOKEN) {
 // Create bot with Telegram token
 export const bot = new Bot(process.env.TELEGRAM_TOKEN)
 
+// webhookCallback will make sure that the correct middleware(listener) function is called
+export const handler = webhookCallback(bot, 'aws-lambda-async');
+
 bot.command("help", async (ctx) => {
   console.log('Received /help command');
   const lines = [
     'Hi! These are the commands (preceded by /) that I know:',
     ' • help - this message',
     ' • menu - show a menu with options',
-    ' • rabo - current Rabobank certificate price',
-    ' • subscribe - subscribe to notifications'
+    ' • rabo - current Rabobank certificate price'
   ];
   await ctx.reply(lines.join('\n'));
 });
@@ -31,55 +34,42 @@ bot.command("menu", async (ctx) => {
   await ctx.reply('Choose an option:', { reply_markup: inlineKeyboard });
 });
 
-bot.command("subscribe", async (ctx) => {
-  console.log('Received /subscibe command');
-  const inlineKeyboard = new InlineKeyboard()
-  .text("Rabobank certificaten", "rabo-subscribe");
-  await ctx.reply('Choose an option:', { reply_markup: inlineKeyboard });
-});
-
-const subscriptions = JSON.parse(fs.readFileSync('./subscriptions.json', 'utf-8'));
-
 bot.callbackQuery('subscribe', async (ctx) => {
   console.log('Received /subscibe callback');
   const inlineKeyboard = new InlineKeyboard();
   
-  subscriptions.forEach((subscription: any) => {
+  // add a button for each subscription
+  subscriptions.forEach((subscription: any, index: number) => {
     inlineKeyboard.text(subscription.name, subscription.key);
+    if ((index + 1) % 2 === 0) {  // Put 2 buttons in one row
+      inlineKeyboard.row();
+    }
   });
 
   await ctx.reply('Choose an option:', { reply_markup: inlineKeyboard });
 });
 
+// create a callback query handler for each subscription
 subscriptions.forEach((subscription: any) => {
   bot.callbackQuery(subscription.key, async (ctx) => {
     console.log(`Received ${subscription.key} callback`);
-    await ctx.reply(subscription.name);
+    const chatId = ctx.chat?.id;
+    
+    // load chat data from S3, add subscription if not already present, and save again
+    try {
+      const chatData = await s3.getChatData(String(ctx.chat?.id));
+      if (!chatData.subscriptions.find(sub => sub.id === subscription.id)) {
+        chatData.subscriptions.push(subscription);
+        await s3.saveChatData(chatData);
+        await ctx.reply(`Chat is geabbonneerd op ${subscription.name} notificaties`);
+      } else {
+        await ctx.reply(`Chat was al geabbonneerd op ${subscription.name} notificaties`);
+      }
+    } catch (error) {
+      console.error(`Error checking/adding ${subscription.key} subscription for chat ${chatId}:`, error);
+    }
   });
 })
-
-bot.callbackQuery('rabo-subscribe', async (ctx) => {
-  const chatId = ctx.chat?.id;
-  const subscription = {id: "600015811", name: "Rabobank certificaten"};
-
-  console.log(`Received /rabo-subscibe callback for chat ${chatId}`);
-  try {
-    const chatData = await s3.getChatData(String(ctx.chat?.id));
-
-    if (!chatData.subscriptions.find(sub => sub.id === subscription.id)) {
-      chatData.subscriptions.push(subscription);
-      await s3.saveChatData(chatData);
-      await ctx.reply('Chat is geabbonneerd op Rabobank certificaten notificaties');
-    } else {
-      await ctx.reply('Chat was al geabbonneerd op Rabobank certificaten notificaties');
-
-    }
-  } catch (error) {
-    console.error(`Error checking/adding rabo subscription for chat ${chatId}:`, error);
-  }
-});
-
-
 
 // Function to fetch Rabobank certificate details and send the response
 const handleRaboCommand: MiddlewareFn<Context> = async (ctx) => {
@@ -101,7 +91,6 @@ bot.command("rabo", handleRaboCommand);
 bot.callbackQuery('rabo', handleRaboCommand);
 bot.callbackQuery('test', (ctx) => ctx.reply('You chose the test option'));
 
-
 // Characters to be escaped
 const escapeCharacters = ['_', '*', '[', ']', '(', ')', '~', '`', '>', '#', '+', '-', '=', '|', '{', '}', '.', '!'];
 
@@ -113,34 +102,6 @@ function escapeSpecialCharacters(string: string): string {
   }
   return newString;
 }
-
-// Subscribe chatId to notifications
-// bot.command('subscribe', async (ctx) => {
-//   const chatId = ctx.chat?.id;
-
-//   // Only add chatId if it is a valid number
-//   if (chatId && !isNaN(chatId)) {
-//     let subscribedChatIds: Set<number> = await s3.getSubscribedChatIds() as Set<number>; // Explicitly type the variable
-
-//     // Log current subscribed chat IDs before adding the new one
-//     console.log(`Current subscribed chat IDs: ${Array.from(subscribedChatIds).join(', ')}`);
-
-//     subscribedChatIds.add(Number(chatId));
-
-//     try {
-//       // Save subscribed chat IDs to S3
-//       await s3.saveSubscribedChatIds(subscribedChatIds);
-//       console.log(`Subscribed chat IDs: ${Array.from(subscribedChatIds).join(', ')}`);
-
-//       await ctx.reply(`Hi! This chat with id ${chatId} has been added to the notification list`);
-//     } catch (error) {
-//       console.error('Error saving subscribed chat IDs:', error);
-//       await ctx.reply('An error occurred while trying to subscribe. Please try again.');
-//     }
-//   } else {
-//     await ctx.reply('Could not find the chat ID. Please try to subscribe again.');
-//   }
-// });
 
 // Function to send current date and time to all subscribed chat IDs
 async function sendNotificationToSubscribers(): Promise<void> {
@@ -168,31 +129,3 @@ async function sendNotificationToSubscribers(): Promise<void> {
   }
 }
 export { sendNotificationToSubscribers}
-
-
-
-// webhookCallback will make sure that the correct middleware(listener) function is called
-export const handler = webhookCallback(bot, 'aws-lambda-async');
-
-// function to set the webhook on Telegram with the API Gateway URL
-async function setTelegramWebhook() {
-
-  // Check if process.env.GW_URL is undefined or empty
-  if (!process.env.GW_URL) {
-    throw new Error('process.env.GW_URL is undefined or empty');
-  }
-
-  try {
-    const webhookUrl = `${process.env.GW_URL}/bot`;
-
-    // Set the Telegram webhook
-    await bot.api.setWebhook(webhookUrl);
-
-    console.log(`Telegram webhook set successfully to: ${webhookUrl}`);
-    return (`Telegram webhook set successfully to: ${webhookUrl}`);
-  } catch (error) {
-    console.error('Error setting Telegram webhook:', error);
-    throw error;
-  }
-}
-export { setTelegramWebhook };
---
+++ src/bot/webhook.ts
@@ -0,0 +1,32 @@
+import { Bot } from 'grammy'
+
+// Check if process.env.TELEGRAM_TOKEN is undefined or empty
+if (!process.env.TELEGRAM_TOKEN) {
+  throw new Error('Telegram token is undefined or empty');
+}
+
+// Create bot with Telegram token
+export const bot = new Bot(process.env.TELEGRAM_TOKEN)
+
+// function to set the webhook on Telegram with the API Gateway URL
+export const setTelegramWebhook = async function setTelegramWebhook() {
+
+  // Check if process.env.GW_URL is undefined or empty
+  if (!process.env.GW_URL) {
+    throw new Error('process.env.GW_URL is undefined or empty');
+  }
+
+  try {
+    const webhookUrl = `${process.env.GW_URL}/bot`;
+
+    // Set the Telegram webhook
+    await bot.api.setWebhook(webhookUrl);
+
+    console.log(`Telegram webhook set successfully to: ${webhookUrl}`);
+    return (`Telegram webhook set successfully to: ${webhookUrl}`);
+  } catch (error) {
+    console.error('Error setting Telegram webhook:', error);
+    throw error;
+  }
+}
+//export { setTelegramWebhook };
